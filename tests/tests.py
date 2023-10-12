a2bb9eacf3858370903bc3c4062185f3f99619ac
Johannes Koester <johannes.koester@uni-due.de>
2023-10-12 11:24:03 +0200

    adapt to changes in test suite

--- tests/tests.py
+++ tests/tests.py
@@ -6,10 +6,11 @@ from snakemake_interface_executor_plugins import ExecutorSettingsBase
 from snakemake_executor_plugin_kubernetes import ExecutorSettings
 
 
-BUCKET_NAME = "snakemake-testing-%s-bucket" % next(tempfile._get_candidate_names())
+BUCKET_NAME = "snakemake-testing-kubernetes-%s-bucket" % next(tempfile._get_candidate_names())
 
 
-class TestWorkflows(snakemake.common.tests.TestWorkflowsBase):
+
+class TestWorkflows(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase):
     __test__ = True
 
     def get_executor(self) -> str:
@@ -17,9 +18,3 @@ class TestWorkflows(snakemake.common.tests.TestWorkflowsBase):
 
     def get_executor_settings(self) -> Optional[ExecutorSettingsBase]:
         return ExecutorSettings()
-
-    def get_default_remote_provider(self) -> Optional[str]:
-        return "S3"
-
-    def get_default_remote_prefix(self) -> Optional[str]:
-        return BUCKET_NAME
