e08fdf8cd37271d3a2e5a06d71b42d179b8c1745
Johannes Koester <johannes.koester@uni-due.de>
2023-10-13 15:33:55 +0200

    update tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 import tempfile
 from typing import Optional
 import snakemake.common.tests
+import snakemake.settings
 from snakemake_interface_executor_plugins import ExecutorSettingsBase
 
 from snakemake_executor_plugin_kubernetes import ExecutorSettings
@@ -19,3 +20,24 @@ class TestWorkflows(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase):
 
     def get_executor_settings(self) -> Optional[ExecutorSettingsBase]:
         return ExecutorSettings()
+
+    def get_assume_shared_fs(self) -> bool:
+        return False
+
+    def get_deployment_settings(
+        self, deployment_method=frozenset()
+    ) -> snakemake.settings.DeploymentSettings:
+        return snakemake.settings.DeploymentSettings(
+            deployment_method=deployment_method,
+            default_storage_provider_auto_deploy=True,
+        )
+
+    def get_remote_execution_settings(
+        self,
+    ) -> snakemake.settings.RemoteExecutionSettings:
+        return snakemake.settings.RemoteExecutionSettings(
+            seconds_between_status_checks=0,
+            envvars=self.get_envvars(),
+            # TODO remove once we have switched to stable snakemake for dev
+            container_image="snakemake/snakemake:latest",
+        )
