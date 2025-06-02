c14da96c8afa3e7b426180b683c7776530ea5ee0
Johannes Koester <johannes.koester@uni-due.de>
2025-06-02 14:39:06 +0200

    fix log message

--- snakemake_executor_plugin_kubernetes/__init__.py
+++ snakemake_executor_plugin_kubernetes/__init__.py
@@ -388,7 +388,7 @@ class Executor(RemoteExecutor):
             self.logger.error(f"Failed to create pod: {e}")
             raise WorkflowError(f"Failed to create pod: {e}")
 
-        self.logger.info("Get status with:\n" "kubectl describe job {jobid}\n")
+        self.logger.info(f"Get status with: kubectl describe job {jobid}")
 
         self.report_job_submission(
             SubmittedJobInfo(job=job, external_jobid=jobid, aux={"pod": pod})
